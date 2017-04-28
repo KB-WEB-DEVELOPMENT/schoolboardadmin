e14b31d4a6f411d06a42e1c1e18bf24105951b6a
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 10:57:16 +0200

    Create register.php

---
+++ resources/lang/de/auth/register.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'register' => 'Registrierung',
+    'first_name' => 'Vorname [ex: Ludwig]',
+    'last_name' => 'Nachname [ex: Müller oder Müller2]',
+    'email' => 'E-Mail Adresse',
+    'password' => 'Kennzeichen',
+    'confirm_password' => 'Kennzeichen bestätigen',
+];
