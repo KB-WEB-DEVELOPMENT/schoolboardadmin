e6f945202533ad5dc96ba5122c1868a8221f3552
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 10:58:45 +0200

    Create changes_grades.php

---
+++ resources/lang/de/back/student/changes_grades.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'edit_title' => 'STUDENT PROFIL ÄNDERN',
+    'first_name' => 'VORNAME',
+    'last_name' => 'NACHNAME',
+    'email' => 'EMAIL',
+    'course_title' => 'KURS TITEL',
+    'current_grade_title' => 'AKTUELLE KURS NOTE',
+    'new_grade_title' => 'NEUE KURS NOTE',
+    'save_new_grade_title' => 'NEUE NOTE SPEICHERN',
+    'student_but_no_course_msg' => 'Der Student hat noch keinen Kurs genommen.',
+
+];
