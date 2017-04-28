f7a84450c540b0427b1a313825e50e247db9d56e
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:47:33 +0200

    Create show.php

---
+++ resources/lang/fr/front/student/profile/show.php
@@ -0,0 +1,13 @@
+  <?php
+
+return [
+
+    'student_profile_title' => "PROFIL DE L'ÉTUDIANT",
+    'first_name' => 'PRÉNOM',
+    'last_name' => 'NOM DE FAMILLE',
+    'email' => 'EMAIL',
+    'course_title' => 'TITRE DU COURS',
+    'grade_title' => 'NOTE POUR LE COURS',
+    'avg_grade_title' => 'MOYENNE PONDÉRÉE',
+    'student_no_course_msg' => "L' étudiant n' a pas encore pris de cours.",
+];
