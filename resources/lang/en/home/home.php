5a43ee1af27a26c40194e008a41b7571b8b398f4
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:35:29 +0200

    Create home.php

---
+++ resources/lang/en/home/home.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'toggle_nav_title' => 'Toggle Navigation',
+    'students_list_title' => 'Students List',
+    'student_search_title' => 'Student Search',
+    'courses_list_title' => 'Courses List',
+    'course_search_title' => 'Course Search',
+    'login_title' => 'Login',
+    'register_title' => 'Register',
+    'my_profile_view_title' => 'My Profile - View',
+    'my_profile_edit_title' => 'My Profile - Edit',
+    'logout_title' => 'Logout',
+
+];
+
