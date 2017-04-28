6acaace6d16c19a6cf4517ee712925e9571f500c
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:36:24 +0200

    Create grade_added.php

---
+++ resources/lang/en/mail/grade_added.php
@@ -0,0 +1,6 @@
+return [
+
+    'greeting' => 'Hello '
+    'messsage' => 'An administrator just graded one of your courses. Your average grade and your profile have been updated.',
+    'ending' => 'Kind Regards,',
+];
