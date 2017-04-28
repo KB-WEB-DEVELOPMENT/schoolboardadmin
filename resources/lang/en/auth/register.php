d027b987025531e2a63b4f00d080af218136070c
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:27:04 +0200

    Create register.php

---
+++ resources/lang/en/auth/register.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'register' => 'Register',
+    'first_name' => 'First Name [ex: John]',
+    'last_name' => 'Last Name [ex: Simpson or Simpson2]',
+    'email' => 'E-Mail Address',
+    'password' => 'Password',
+    'confirm_password' => 'Confirm Password',
+];
