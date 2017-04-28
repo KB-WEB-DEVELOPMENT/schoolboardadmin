0ddd8c4fb4a839ad6ca976964cea45e7814b49a1
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 12:04:05 +0200

    Create added_grade.blade.php

---
+++ resources/views/back/student/added_grade.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading"> trans('back/student/added_grade.title_msg')  </div>
+
+                <div class="panel-body">
+
+                    @if ($message = 1)
+
+                        trans('back/student/added_grade.grade_added_msg').
+					
+                    @else
+
+                        trans('back/student/added_grade.already_grade_for_course_msg').
+
+                    @endif
+				                
+				</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+
