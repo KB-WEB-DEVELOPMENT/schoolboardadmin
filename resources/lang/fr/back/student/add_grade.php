edd64ee1c2a86d1f4962269b80596b573c0578f5
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:41:19 +0200

    Create add_grade.php

---
+++ resources/lang/fr/back/student/add_grade.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'add_grade_msg' => 'Ajouter une note de cours à un étudiant',
+    'select_student' => 'Sélectionnez un étudiant',
+    'select_course' => 'Sélectionnez un cours',
+    'select_grade' => "Choisir la note de l' étudiant pour le cours",
+    'submit' => 'Envoi',
+    'no_student_nor_course_msg' => "Désolé, il n' y a soit pas d' étudiants inscrits, soit pas de cours disponibles.",
+
+];
