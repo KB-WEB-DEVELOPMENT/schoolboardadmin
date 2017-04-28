1ee95c982880d86e23ea34b97f1147176453e3bf
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:09:23 +0200

    Create passwords.php

---
+++ resources/lang/de/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Kennwörter müssen mindestens sechs Zeichen lang sein und der Auftragsbestätigung übereinstimmen.',
+    'reset' => 'Ihr Passwort wurde zurückgesetzt!',
+    'sent' => 'Wir haben e-Passwort zurücksetzen Link gemailt!',
+    'token' => 'Dieses Passwort Token zurückgesetzt ist ungültig.',
+    'user' => "Wir können nicht ein Benutzer mit dieser E-Mail Adresse finden.",
+
+];
