2c840bfdca4a7c6320c3df4bb1a20363f50abc5e
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:00:14 +0200

    Create course.php

---
+++ resources/lang/de/back/course.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    'course_update_title' => 'KURS AKTUALISIEREN',
+    'course_added_msg' => 'Der Kurs wurde aktualisert.',
+    'add_course_msg' => 'Ein neuer Kurs einfügen',
+    'course_title' => 'Kurs Titel',
+    'enter_course_title_msg' => 'Fügen Sie einen neuen Kurs Titel bitte ein',
+    'no_unique_course_title_msg' => 'Der Kurs Titel muss nicht einzigartig sein. [z.B: Biologie, Mathematik, ...]',
+    'course_number_title' => 'Kurs Nummer',
+    'course_number_msg' => 'Fügen Sie eine Kurs Nummer bitte ein',
+    'unique_course_number_msg' => 'Die Kurs Nummer muss einzigartig sein. [z.B: 235, 149, ...]',
+    'course_description_msg' => 'Fügen Sie eine neue Kurs Beschreibung bitte ein.',
+    'course_description_title' => 'Kurs Beschreibung',
+    'course_availability_title' => 'Kurs Verfügbarkeit',
+    'course_available_msg' => 'Bitte wählen Sie ob der Kurs verfügbar oder unverfügbar ist.',
+    'add_new_course_msg' => 'Neuer Kurs einfügen',
+    'edit_course_title' => 'Kurs Details ändern',
+    'submit_changes_title' => 'Änderungen eingeben',
+    'course_addition_title' => 'KURS EINFÜGEN',
+    'course_added_msg' => 'Der Kurs wurde in der Datenbank wurde angelegt',
+];
