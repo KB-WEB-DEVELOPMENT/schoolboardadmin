60af441ff09100490d898bf9c1ec922de4df9e91
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:46:56 +0200

    Create list.php

---
+++ resources/lang/fr/front/courses/list.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'courses_title' => 'COURS',
+    'course_title' => 'TITRE DU COURS (ASC)',
+    'course_number_title' => 'NUMÉRO DU COURS',
+    'course_details_title' => 'DÉTAILS DU COURS',
+    'edit_course_title' => 'MODIFIER LES DÉTAILS DU COURS',
+    'course_details_title' => 'détails du cours',
+    'edit_course_txt' => 'modifier les détails du cours',
+    'no_courses_available_msg' => "Il n' y actuellement aucun cours disponible.",
+];
