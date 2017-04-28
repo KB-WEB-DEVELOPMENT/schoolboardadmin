c463007d28db0ff6c7dd33f5fa2df675de1aeb82
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:27:54 +0200

    Create added.grade.php

---
+++ resources/lang/en/back/student/added.grade.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'title_msg' => 'GIVING A GRADE TO A STUDENT FOR A COURSE',
+    'already_grade_for_course_msg' => 'Sorry, the student has already received a grade for this course',
+    'grade_added_msg' => 'The student grade for this course was successfully added to the database',
+
+];
