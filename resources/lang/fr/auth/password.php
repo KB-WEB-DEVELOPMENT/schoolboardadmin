dd11606341701117c65bda4b946225420f90c580
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:40:34 +0200

    Create password.php

---
+++ resources/lang/fr/auth/password.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    'reset_password' => 'Créer un nouveau mot de passe',
+    'email' => 'email',
+    'send_password_link' => 'Envoyer par email un lien pour créer un nouveau mot de passe',
+];
