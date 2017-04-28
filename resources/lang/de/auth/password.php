644888180966a33c33f145ca595e68a447749830
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 10:56:56 +0200

    Create password.php

---
+++ resources/lang/de/auth/password.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    'reset_password' => 'Kennzeichen zurücksetzen',
+    'email' => 'E-Mail Adresse',
+    'send_password_link' => 'Link um Passwort zurückzusetzen schicken',
+];
