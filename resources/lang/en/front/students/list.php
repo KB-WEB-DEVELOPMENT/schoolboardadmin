743fe7b6c46e42bff3c988931e9111d293335a1f
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:35:04 +0200

    Create list.php

---
+++ resources/lang/en/front/students/list.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'list_students_title' => 'LIST OF STUDENTS',
+    'first_name' => 'FIRSTNAME (ASC)',
+    'last_name' => 'LASTNAME',
+    'view_profile_title' => 'VIEW PROFILE',
+    'edit_profile_title' => 'EDIT PROFILE',
+    'delete_title' => 'DELETE',
+    'view_profile_msg' => 'view profile',
+    'edit_profile_msg' => 'edit profile',
+    'delete_profile_msg' => 'delete profile',
+    'no_reg_students_msg' => 'There are currently no registered students',
+];
