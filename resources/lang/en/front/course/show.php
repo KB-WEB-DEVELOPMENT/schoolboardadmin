3223bc68edbe803d8d40a785feebaa1ac3cfe87b
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:32:54 +0200

    Create show.php

---
+++ resources/lang/en/front/course/show.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'course_details_title' => 'COURSE DETAILS',
+    'course_title_title' => 'COURSE TITLE',
+    'course_number_title' => 'COURSE NUMBER',
+    'course_description_title' => 'COURSE DESCRIPTION',
+    'course_availability_title' => 'AVAILABILITY',
+];
