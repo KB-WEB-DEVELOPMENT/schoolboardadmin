bc79a4dd9d49479c41405d695b668be75839cce9
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 10:56:30 +0200

    Create login.php

---
+++ resources/lang/de/auth/login.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'login' => 'SICH ANMELDEN',
+    'email' => 'Ihre Email Adresse',
+    'password' => 'Kennzeichen',
+    'remember_me' => 'Auf diesem Computer merken',
+    'forgot_password' => 'Kennzeichen vergessen ?',
+];
