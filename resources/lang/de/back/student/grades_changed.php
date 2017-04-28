61866f48190109f3c03742ce154a3db7edd76677
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 10:59:34 +0200

    Create grades_changed.php

---
+++ resources/lang/de/back/student/grades_changed.php
@@ -0,0 +1,7 @@
+  <?php
+
+return [
+
+    'update_grades_title' => "STUDENT KURS NOTEN AKTUALISIEREN",
+    'updated_grades_msg' => 'Die Kurs Noten wurden aktualisiert und alle Änderungen in der Datenbank angelegt.',
+];
