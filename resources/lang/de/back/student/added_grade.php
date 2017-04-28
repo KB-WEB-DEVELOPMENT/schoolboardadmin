32b329cde31accae34072b2509c7c2ce7b1d30f0
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 10:58:19 +0200

    Create added_grade.php

---
+++ resources/lang/de/back/student/added_grade.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'title_msg' => 'EINE KURS NOTE FÜR EINEN STUDENT EINGEBEN',
+    'already_grade_for_course_msg' => 'Der Student hat für diesen Kurs schon eine Note bekommen.',
+    'grade_added_msg' => 'Die Student Note für diesen Kurs wurde in der Datebank angelegt.',
+
+];
