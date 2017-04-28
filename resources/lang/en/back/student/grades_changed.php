263450e0895d7e20132be5caa17b02741da9834e
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:29:48 +0200

    Create grades_changed.php

---
+++ resources/lang/en/back/student/grades_changed.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+
+    'update_grades_title' => "UPDATING A STUDENT'S GRADES",
+    'updated_grades_msg' => 'The grades have been updated and the changes made to the database.',
+];
