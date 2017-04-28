e79139748d1d61aa20ce334ce7a9682ee727a38f
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:45:52 +0200

    Create found.php

---
+++ resources/lang/fr/front/course/found.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'searched_course_results_title' => 'RECHERCHE DE COURS: RÉSULTATS',
+    'course_title' => 'TITRE DU COURS(ASC)',
+    'course_number_title' => 'NUMÉRO DU COURS',
+    'course_details_title' => 'DÉTAILS',
+    'edit_course_title' => 'MODIFIER LE COURS',
+    'course_details_title' => 'détails du cours',
+    'edit_course_title' => 'modifier le cours',
+    'course_search_no_result_msg' => "Votre recherche n' a produit aucun résultats.",
+];
