223f2e556657ff774c0e9ed1bc56feffb546f432
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:34:20 +0200

    Create found.php

---
+++ resources/lang/en/front/student/found.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'searched_student_results_title' => 'SEARCHED STUDENT: RESULTS',
+    'first_name' => 'FIRSTNAME',
+    'last_name' => 'LASTNAME',
+    'view_profile_title' => 'VIEW PROFILE',
+    'edit_profile_title' => 'EDIT PROFILE',
+    'delete_title' => 'DELETE',
+    'view_profile_title' => 'view profile',
+    'edit_profile_title' => 'edit profile',
+    'delete_profile_title' => 'delete profile',
+    'student_search_no_result_msg' => 'The student search did not yield any results',
+];
