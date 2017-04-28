c917c2070a013882a53e71534dad674c99d68f14
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:50:23 +0200

    Create grade_added.php

---
+++ resources/lang/fr/mail/grade_added.php
@@ -0,0 +1,6 @@
+return [
+
+    'greeting' => 'Bonjour '
+    'messsage' => 'Un administrateur a ajouté une note de cours et votre moyenne a été mise à jour. Veuillez consulter votre profil.',
+    'ending' => 'Cordialement,',
+];
