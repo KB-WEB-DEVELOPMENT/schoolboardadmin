f27433b3a92908a10dda36776a8499ec3e4e20a3
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 10:27:55 +0200

    Create 2017_17_03_1000000_create_courses_students_table.php

---
+++ database/migrations/2017_17_03_1000000_create_courses_students_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCourseStudentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course_student', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('course_id');
+            $table->integer('student_id');
+            $table->integer('course_grade');
+            $table->timestamps();
+
+            $table->foreign('course_id')->unsigned()->references('id')->on('course')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('student_id')->unsigned()->references('id')->on('user')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('StudentsCoursesGrades');
+    }
+}
