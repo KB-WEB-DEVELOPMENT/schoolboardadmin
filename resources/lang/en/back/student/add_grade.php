1b375b007f77e47ae166c5b4438ebd2ec61179d3
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:27:34 +0200

    Create add_grade.php

---
+++ resources/lang/en/back/student/add_grade.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'add_grade_msg' => 'Add a course grade to a student',
+    'select_student' => 'Select a student',
+    'select_course' => 'Select a course',
+    'select_grade' => "Select the student's grade for this course",
+    'submit' => 'Submit',
+    'no_student_nor_course_msg' => 'Sorry, there are either no registered students or no registered courses',
+
+];
