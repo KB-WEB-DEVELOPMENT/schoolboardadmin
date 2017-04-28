497d56109a57d36df2f62583f9d138dec796627a
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:30:09 +0200

    Create course.php

---
+++ resources/lang/en/back/course.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    'course_update_title' => 'COURSE UPDATE',
+    'course_added_msg' => 'The course was successfuly updated.',
+    'add_course_msg' => 'Add a new course',
+    'course_title' => 'Course title',
+    'enter_course_title_msg' => 'Please enter the title of the course',
+    'no_unique_course_title_msg' => 'The course title does not have to be unique. [EX: HISTORY, MATHEMATICS, ...]',
+    'course_number_title' => 'Course number',
+    'course_number_msg' => 'Please enter the course number',
+    'unique_course_number_msg' => 'The course number must be unique. [EX: 235, 149, ...]',
+    'course_description_msg' => 'Please enter a brief description of the course.',
+    'course_description_title' => 'Course description',
+    'course_availability_title' => 'Course availability',
+    'course_available_msg' => 'Choose if the course is currently available or not.',
+    'add_new_course_msg' => 'Add a new course',
+    'edit_course_title' => 'EDIT COURSE',
+    'submit_changes_title' => 'Submit changes',
+    'course_addition_title' => 'COURSE ADDITION',
+    'course_added_msg' => 'The course was successfully added to the database',
+];
