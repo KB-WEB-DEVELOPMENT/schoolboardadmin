a7fc16cb5ec6dd83b4f2a664181716d72a7c66e2
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:33:54 +0200

    Create show.php

---
+++ resources/lang/en/front/student/profile/show.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'student_profile_title' => 'STUDENT PROFILE',
+    'first_name' => 'FIRST NAME',
+    'last_name' => 'LAST NAME',
+    'email' => 'EMAIL',
+    'course_title' => 'COURSE',
+    'grade_title' => 'GRADE',
+    'avg_grade_title' => 'AVERAGE GRADE',
+    'student_no_course_msg' => 'The student has not taken any courses yet',
+];
