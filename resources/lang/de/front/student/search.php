252cdf338bbea589c26f92e92b4f253175297c5d
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:05:40 +0200

    Create search.php

---
+++ resources/lang/de/front/student/search.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'search_student_msg' => 'Ein Student suchen und finden',
+    'student_first_name_title' => 'Der Student Vorname (optional)',
+    'student_first_name_msg' => 'Bitte tragen Sie der Student Vorname den Sie suchen',
+    'student_last_name_title' => 'Der Student Nachname (erforderlich)',
+    'student_last_name_msg' => 'Bitte tragen Sie der Student Nachname den Sie suchen',
+    'search_student_msg' => 'Ein Student suchen und finden',
+];
