3436006c0c6a1418273a593055f3bedfba2e8e85
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:06:44 +0200

    Create home.php

---
+++ resources/lang/de/home/home.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'toggle_nav_title' => 'Menu',
+    'students_list_title' => 'Studenten Liste',
+    'student_search_title' => 'Student suchen',
+    'courses_list_title' => 'Kursen Liste',
+    'course_search_title' => 'Kursen suchen',
+    'login_title' => 'Anmelden',
+    'register_title' => 'Registrieren',
+    'my_profile_view_title' => 'Mein Profil - anschauen',
+    'my_profile_edit_title' => 'Mein Profil - ändern',
+    'logout_title' => 'abmelden',
+
+];
+
