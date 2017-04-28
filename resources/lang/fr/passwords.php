30cffea04ed31fb4ad2f5956f284fb94eb3f518e
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:51:49 +0200

    Create passwords.php

---
+++ resources/lang/fr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Votre mot de passe doit contenir au moins 6 caractères et doit être confirmé.',
+    'reset' => 'Un nouveau mot de passe a été créé pour vous.',
+    'sent' => 'Nous vous avons envoyé par email un lien vous permettant de créer un nouveau mot de passe.',
+    'token' => "Ce token pour créer un nouveau mot de passe n' est pas valable.",
+    'user' => "L' utilisateur avec cette adresse email n' est pas détecté.",
+
+];
