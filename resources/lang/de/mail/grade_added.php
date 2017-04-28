0f4beaaaaae072d43f17e990f74f30b91b14b1e7
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:07:39 +0200

    Create grade_added.php

---
+++ resources/lang/de/mail/grade_added.php
@@ -0,0 +1,6 @@
+return [
+
+    'greeting' => 'Hallo'
+    'messsage' => 'Ein Administrator hat für Sie eine neue Kurs Note eingefügt. Ihre Durschnitte Note wurde wieder kalkuliert und ist unter Ihrem Profil sichtbar.',
+    'ending' => 'Mit freundlichen Grüßen,',
+];
