10fdb8e0763de7f56f4d8a15b764162ee63757d7
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:32:09 +0200

    Create found.php

---
+++ resources/lang/en/front/course/found.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'searched_course_results_title' => 'SEARCHED COURSE: RESULTS',
+    'course_title' => 'COURSE TITLE (ASC)',
+    'course_number_title' => 'COURSE NUMBER',
+    'course_details_title' => 'COURSE DETAILS',
+    'edit_course_title' => 'EDIT COURSE',
+    'course_details_title' => 'course details',
+    'edit_course_title' => 'edit course',
+    'course_search_no_result_msg' => "Your course search didn't yield any results",
+];
