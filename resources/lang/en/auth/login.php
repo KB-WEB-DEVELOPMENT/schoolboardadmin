0b6779790d4c334bae925e7b771917a61b7c3e90
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:26:05 +0200

    Create login.php

---
+++ resources/lang/en/auth/login.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'login' => 'LOGIN',
+    'email' => 'E-Mail Address',
+    'password' => 'Password',
+    'remember_me' => 'Remember Me',
+    'forgot_password' => 'Forgot Your Password?',
+];
