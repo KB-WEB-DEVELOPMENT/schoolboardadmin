9f74040de57511e3fc2568522470ced7e853e017
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 12:06:29 +0200

    Create grades_changed.blade.php

---
+++ resources/views/back/student/grades_changed.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading"> trans('back/student/grades_changed.update_grades_title') </div>
+
+                <div class="panel-body">
+					
+					trans('back/student/grades_changed.updated_grades_msg').	
+				                
+				</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+
