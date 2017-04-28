dcc8eb9e4baacb3325ae2abdf8790c84067ef8bd
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 10:03:29 +0200

    Create SearchStudentFormRequest.php

---
+++ app/Http/Requests/SearchStudentFormRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SearchStudentFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+  public function rules()
+    {
+       
+        return [
+            'first_name' => 'bail|alpha|min:3|max:50',
+            'last_name' => 'bail||required|alpha_num|min:3|max:50',
+        ];
+    }
+}
