5477671346262618a0dc607bbcd158e31ef3b2ea
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:32:35 +0200

    Create search.php

---
+++ resources/lang/en/front/course/search.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'search_course_msg' => 'Search a course',
+    'course_title' => 'Course title (required)',
+    'course_title_msg' => 'Enter the title of the course you are searching for',
+    'course_title_error_msg' => 'Enter the title of the course you are searching for. [EX: HISTORY, MATHEMATICS, ...]',
+    'course_number_title' => 'Course number (optional)',
+    'course_number_msg' => 'Enter the course number of the course you are searching for',
+    'course_number_unique_msg' => 'The course number must be unique. [EX: 235, 149, ...]',
+    'search_course_msg' => 'Search course',
+
+];
