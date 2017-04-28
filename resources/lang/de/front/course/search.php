d8afc3b15b6240c33d6fad33c2abdaa05b8472c4
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:03:14 +0200

    Create search.php

---
+++ resources/lang/de/front/course/search.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'search_course_msg' => 'Ein Kurs suchen',
+    'course_title' => 'Kurs Titel (erforderlich)',
+    'course_title_msg' => 'Bitte geben Sie den gesuchten Kurs Title ein',
+    'course_title_error_msg' => 'Bitte geben Sie den gesuchten Kurs Titel ein. [z.B: Biologie, Mathematik, ...]',
+    'course_number_title' => 'Kurs Nummer (optional)',
+    'course_number_msg' => 'Bitte geben Sie die gesuchte Kurs Nummer ein',
+    'course_number_unique_msg' => 'Die Kurstitle muss einziartig sein. [z.B: 235, 149, ...]',
+    'search_course_msg' => 'Kurs suchen',
+
+];
