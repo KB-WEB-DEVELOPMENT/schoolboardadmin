733e6214fcc5736e103304c2a450fbc76edb7d25
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 11:49:45 +0200

    Create app.php

---
+++ resources/lang/fr/layouts/app.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    
+    'language_iso' => 'fr',  
+    'toggle_nav_title' => 'Menu',
+    'students_list_title' => 'Liste des étudiants',
+    'student_search_title' => 'Rechercher un étudiant',
+    'courses_list_title' => 'Liste des cours',
+    'course_search_title' => 'Rechercher un cours',
+    'login_title' => 'Se connecter',
+    'register_title' => "S' inscrire",
+    'my_profile_view_title' => 'Mon profil - Visualiser',
+    'my_profile_edit_title' => 'My Profile - Modifier',
+    'logout_title' => 'Se déconnecter',
+
+];
