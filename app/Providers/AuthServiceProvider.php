09740439308a17303aa9cac10f463dfce3a119b2
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-04-28 10:11:34 +0200

    Create AuthServiceProvider.php

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use App\Student;
+use App\Course;
+use App\Policies\StudentPolicy;
+use App\Policies\CoursePolicy;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'App\Model' => 'App\Policies\ModelPolicy',
+        Student::class => StudentPolicy::class,
+        Course::class =>  CoursePolicy::class,
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+
+    }
+}
